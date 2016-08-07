ab27c130e19dd6edbe4fa8aa90735cef85f5092b
hhwwbbb <395953134@qq.com>
2016-08-07 18:24:59 +0800

    Adjust the package structure

--- src/main/java/com/example/lbf/imatationofwechat/chat/ChatFragment.java
+++ src/main/java/com/example/lbf/imatationofwechat/chat/ChatFragment.java
@@ -7,6 +7,7 @@ import android.animation.ObjectAnimator;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.os.Bundle;
@@ -18,6 +19,7 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.SpannableString;
 import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -30,10 +32,11 @@ import android.view.WindowManager;
 import android.view.animation.DecelerateInterpolator;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import com.example.lbf.imatationofwechat.Activity.TextViewFragment;
 import com.example.lbf.imatationofwechat.R;
 import com.example.lbf.imatationofwechat.util.CommonUtil;
+import com.example.lbf.imatationofwechat.util.EmojiUtil;
 import com.example.lbf.imatationofwechat.util.InputMethodUtil;
 import com.example.lbf.imatationofwechat.util.LogUtil;
 import com.example.lbf.imatationofwechat.adapter.EmojiPagerAdapter;
@@ -673,6 +676,49 @@ public class ChatFragment extends Fragment implements ChatContract.View,View.OnC
                 mPresenter.sendMessage(textInputView.getText().toString(), WeChatPersistenceContract.MsgsEntry.MSG_TYPE_TEXT);
                 break;
 
+        }
+    }
+
+    /**
+     * Created by lbf on 2016/7/11.
+     */
+    public static class TextViewFragment extends Fragment implements View.OnTouchListener{
+        public static final String INTENT_KEY_INFO = "info";
+        private TextView textView;
+        public TextViewFragment() {
+        }
+
+        @Nullable
+        @Override
+        public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+            textView = new TextView(getActivity());
+            textView.setWidth(ViewGroup.LayoutParams.MATCH_PARENT);
+            textView.setHeight(ViewGroup.LayoutParams.MATCH_PARENT);
+            textView.setGravity(Gravity.CENTER);
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP,25);
+            textView.setTextColor(Color.BLACK);
+            textView.setBackgroundColor(Color.WHITE);
+            textView.setClickable(true);
+            textView.setOnTouchListener(this);
+            return textView;
+        }
+
+        @Override
+        public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+            super.onActivityCreated(savedInstanceState);
+            Bundle bundle = getArguments();
+            String content = bundle.getString("info");
+            SpannableString spannableString = EmojiUtil.convertString(getActivity(),content);
+            textView.setText(spannableString);
+        }
+
+        @Override
+        public boolean onTouch(View v, MotionEvent event) {
+            if(event.getAction() == MotionEvent.ACTION_UP){
+                getActivity().getSupportFragmentManager().popBackStack();
+                return true;
+            }
+            return false;
         }
     }
 }
